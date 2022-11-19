4ee30f4075ab775934fb46b6dd06821abf1c92f8
Noman Sanjari <nomansanjari2001@gmail.com>
2022-11-19 14:30:19 -0700

    Added .env for security

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
 
 // default port to listen on
 const PORT = 8000;
 
+// so we can use json to send data back and forth
+app.use(express.json());
+
+// so we can encode data in the url too
+app.use(bodyParser.urlencoded({extended: false}));
+
 // home page -> /
-app.get('/', (request, response) => {
+app.get('/', async (request, response) => {
     response.send("Suh dude!");
     console.log("Traffic received!");
 });
